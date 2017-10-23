0c6b5ce96069d0ab9c52fb8c71a00dc24dc37412
XhaHilger <XhaHilger@Kraven>
2017-10-22 20:56:30 -0400

    Merge origin/master
    Conflicts:
    	.gitignore
    	README.md

---
+++ common/config/main-local.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            //'dsn' => 'mysql:host=localhost;dbname=web_innova',
+            'dsn' => 'sqlsrv:server=localhost;Database=AEINVERSORA', // MS SQL Server, dblib driver
+            'username' => 'sa',
+            'password' => 'sql2008SA',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
