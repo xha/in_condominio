9a35125a0bab332bcb2ddd3e366abb09a861df31
xha <sirit20@gmail.com>
2017-11-01 15:45:07 -0430

    Despachos

--- common/models/AccessHelpers.php
+++ common/models/AccessHelpers.php
@@ -28,10 +28,9 @@ class AccessHelpers {
     public function chequeo() {
         $operacion = str_replace("/", "-", Yii::$app->controller->route);
         $permitirSiempre = ['site-index','site-error','site-logout','site-login','site-register','site-recuperar','site-cambiar'];
-        if (in_array($operacion, $permitirSiempre)) {
-            return true;
-        }
-        return true;
+
+        if (in_array($operacion, $permitirSiempre)) return true;
+        if (strpos($operacion, 'busca')) return true;
         $rol = Yii::$app->user->identity->id_rol;
         if (!AccessHelpers::getAcceso($operacion,$rol)) {
             echo $this->render('../no_permitido');
